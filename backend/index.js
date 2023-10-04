d5681071cf12c40e5e95fde813861289ce9ec4d6
Prathamesh Dhananjay Kadve <prathameshdk02@gmail.com>
2023-10-04 11:07:28 +0530

    Updated Hash Generation

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,35 @@ require('dotenv').config();
 // Critical Imports Here...
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+// Parsing Request Bodies...
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 // Handling Requests here...
 app.get('/', (req, res) => {
+    console.log(req.body);
     res.send('<h1>Welcome to our Backend.</h1>');
+});
+
+/* 
+    Parse incoming request body.
+    - Accept list of elements.
+    - return the results as json.
+
+*/ 
+app.post('/scan', (req, res) => {
+    let count = req.body.data.length;
+    console.log(req.body.data);
+    res.send(
+        JSON.stringify({
+            results: 'Received.',
+            count: count,
+        })
+    );
 });
 
 // MongoDB Connection & App Listen Stuff...
@@ -21,7 +44,6 @@ const mongooseOptions = {
 };
 
 mongoose.connect(process.env.MONGODB_URI, mongooseOptions).then(() => {
-    
     // Listening for Requests...
     app.listen(process.env.PORT, () => {
         console.log('Started API...');
--- frontend/gen/generate-signatures.py
+++ frontend/gen/generate-signatures.py
@@ -1,11 +1,16 @@
 from pathlib import Path
+from datetime import datetime
 import hashlib
+import json
+import requests
 
-samplePath = './liveSamples/'
+samplePath = __file__ + '/../liveSamples'
 
 samples = [file for file in Path(samplePath).iterdir() if file.is_file()]
 sampleCount = len(samples)
 hashGenerations = 0
+
+obj_list = []
 
 print(f"\nFile Hashes from \"{samplePath}\":")
 
@@ -13,8 +18,41 @@ for sample in samples:
     try:
         hash = hashlib.sha256(Path(sample).read_bytes()).hexdigest()
         hashGenerations += 1
-        print(hash)
+        obj = {
+            'name': sample.name,
+            'extension': sample.suffix,
+            'size': sample.stat().st_size,
+            'creation-time': datetime.strftime(datetime.fromtimestamp(sample.stat().st_ctime),"%Y-%M-%d"),
+            'hash': hash   
+        }
+        obj_list.append(obj)
     except Exception as e:
         print(e)
 
 print(f"\nGenerated over {hashGenerations} hashes from {sampleCount} samples.\nEncountered Errors were {sampleCount - hashGenerations}")
+
+final_obj = {
+    'data': obj_list
+}
+
+with open("sampleData.txt","w") as sampleData:
+    json.dump(final_obj,sampleData)
+
+
+# json_obj = json.dumps(final_obj,indent=4)
+
+# print(json_obj)
+
+
+# response = requests.post('http://localhost:3000/scan',json=final_obj)
+# print(response)
+
+
+
+
+
+
+
+
+
+    
